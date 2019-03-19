8fae1fbd06ee557fb4a88b877ac5ebd9396b04f2
Osman Nuri Okumus <onokumus@gmail.com>
2019-03-19 11:52:05 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,137 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MetisMenu from 'metismenujs';
+import 'metismenujs/dist/metismenujs.css';
 
-class App extends Component {
+class MM extends Component {
+  componentDidMount() {
+    this.$el = this.el;
+    this.mm = new MetisMenu(this.$el);
+  }
+  componentWillUnmount() {
+    this.mm('dispose');
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="mm-wrapper">
+        <ul className="metismenu" ref={el => this.el = el}>
+          {this.props.children}
+        </ul>
       </div>
+    );
+  }
+}
+
+class App extends Component {
+
+  render() {
+    return (
+      <MM>
+        <li>
+          <a className="has-arrow" href="#" aria-expanded="false">
+            <span className="fa fa-fw fa-github fa-lg"></span>
+            metisMenu
+        </a>
+          <ul>
+            <li>
+              <a href="https://github.com/onokumus/metismenujs">
+                <span className="fa fa-fw fa-code-fork"></span> Fork
+            </a>
+            </li>
+            <li>
+              <a href="https://github.com/onokumus/metismenujs">
+                <span className="fa fa-fw fa-star"></span> Star
+            </a>
+            </li>
+            <li>
+              <a href="https://github.com/onokumus/metismenujs/issues">
+                <span className="fa fa-fw fa-exclamation-triangle"></span> Issues
+            </a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a className="has-arrow" href="#" aria-expanded="false">Menu 0</a>
+          <ul>
+            <li>
+              <a href="#">item 0.1</a>
+            </li>
+            <li>
+              <a href="#">item 0.2</a>
+            </li>
+            <li>
+              <a href="http://onokumus.com">onokumus</a>
+            </li>
+            <li>
+              <a href="#">item 0.4</a>
+            </li>
+          </ul>
+        </li>
+        <li id="removable">
+          <a className="has-arrow" href="#" aria-expanded="false">Menu 1</a>
+          <ul>
+            <li>
+              <a href="#">item 1.1</a>
+            </li>
+            <li>
+              <a href="#">item 1.2</a>
+            </li>
+            <li>
+              <a className="has-arrow" href="#" aria-expanded="false">Menu 1.3</a>
+              <ul>
+                <li>
+                  <a href="#">item 1.3.1</a>
+                </li>
+                <li>
+                  <a href="#">item 1.3.2</a>
+                </li>
+                <li>
+                  <a href="#">item 1.3.3</a>
+                </li>
+                <li>
+                  <a href="#">item 1.3.4</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">item 1.4</a>
+            </li>
+            <li>
+              <a className="has-arrow" href="#" aria-expanded="false">Menu 1.5</a>
+              <ul>
+                <li>
+                  <a href="#">item 1.5.1</a>
+                </li>
+                <li>
+                  <a href="#">item 1.5.2</a>
+                </li>
+                <li>
+                  <a href="#">item 1.5.3</a>
+                </li>
+                <li>
+                  <a href="#">item 1.5.4</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a className="has-arrow" href="#" aria-expanded="false">Menu 2</a>
+          <ul>
+            <li>
+              <a href="#">item 2.1</a>
+            </li>
+            <li>
+              <a href="#">item 2.2</a>
+            </li>
+            <li>
+              <a href="#">item 2.3</a>
+            </li>
+            <li>
+              <a href="#">item 2.4</a>
+            </li>
+          </ul>
+        </li>
+      </MM>
     );
   }
 }
